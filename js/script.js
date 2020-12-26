1a1d9afea714551685c56ca264e94f5765a19b19
unknown <urustamov756@.gmail.com>
2020-12-26 18:17:23 +0500

    bank

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// alert('ulandi')
+let burger = document.querySelector('#hamburger');
+let bodyimg = document.querySelector('.bodyimg');
+
+
+burger.addEventListener('click', function () {
+burger.classList.toggle('clicked')
+bodyimg.classList.toggle('clicked')
+})
+
